1e15e290477a2b7570b8165f557e4d79ee2d06b3
gabeschine <gabe@schine.net>
2025-04-11 15:13:06 -0400

    format code

--- vt2geojson/features.py
+++ vt2geojson/features.py
@@ -3,13 +3,13 @@ from math import pi, atan, exp
 
 
 class GeometryType(Enum):
-    UNKNOWN = 'Unknown'
-    POINT = 'Point'
-    LINESTRING = 'LineString'
-    POLYGON = 'Polygon'
-    MULTILINESTRING = 'MultiLineString'
-    MULTIPOLYGON = 'MultiPolygon'
-    MULTIPOINT = 'MultiPoint'
+    UNKNOWN = "Unknown"
+    POINT = "Point"
+    LINESTRING = "LineString"
+    POLYGON = "Polygon"
+    MULTILINESTRING = "MultiLineString"
+    MULTIPOLYGON = "MultiPolygon"
+    MULTIPOINT = "MultiPoint"
 
 
 class Feature:
@@ -22,29 +22,31 @@ class Feature:
 
     @property
     def tiles_coordinates(self):
-        return self.obj['geometry']['coordinates']
+        return self.obj["geometry"]["coordinates"]
 
     @property
     def geometry_type(self):
-        return GeometryType(self.obj['geometry']['type'])
+        return GeometryType(self.obj["geometry"]["type"])
 
     @property
     def properties(self):
-        return self.obj['properties']
+        return self.obj["properties"]
 
     def toGeoJSON(self):
-        size = self.extent * 2 ** self.z
+        size = self.extent * 2**self.z
         x0 = self.extent * self.x
         y0 = self.extent * self.y
 
         def project_one(p_x, p_y):
-            y2 = 180 - (p_y + y0) * 360. / size
-            long_res = (p_x + x0) * 360. / size - 180
-            lat_res = 360. / pi * atan(exp(y2 * pi / 180)) - 90
+            y2 = 180 - (p_y + y0) * 360.0 / size
+            long_res = (p_x + x0) * 360.0 / size - 180
+            lat_res = 360.0 / pi * atan(exp(y2 * pi / 180)) - 90
             return [long_res, lat_res]
 
         def project(coords):
-            if coords and all(isinstance(x, int) or isinstance(x, float) for x in coords):
+            if coords and all(
+                isinstance(x, int) or isinstance(x, float) for x in coords
+            ):
                 assert len(coords) == 2
                 return project_one(coords[0], coords[1])
             return [project(l) for l in coords]
@@ -63,11 +65,9 @@ class Feature:
 
         result = {
             "type": "Feature",
-            "geometry": {
-                "type": geometry_type,
-                "coordinates": coords
-            },
-            "properties": self.properties
+            **id_attrs,
+            "geometry": {"type": geometry_type, "coordinates": coords},
+            "properties": self.properties,
         }
         return result
 
@@ -82,11 +82,15 @@ class Layer:
 
     @property
     def extent(self):
-        return self.obj['extent']
+        return self.obj["extent"]
 
     def toGeoJSON(self):
         return {
             "type": "FeatureCollection",
-            "features": [Feature(x=self.x, y=self.y, z=self.z, obj=f, extent=self.extent).toGeoJSON()
-                         for f in self.obj['features']]
+            "features": [
+                Feature(
+                    x=self.x, y=self.y, z=self.z, obj=f, extent=self.extent
+                ).toGeoJSON()
+                for f in self.obj["features"]
+            ],
         }
